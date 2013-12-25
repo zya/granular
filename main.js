7c1fcaab00a75f67bbc1f2242699b19b09947a0b
zya <ziyaeddiny@gmail.com>
2013-12-25 15:59:25 +0000

    Stable - But move not working

--- main.js
+++ main.js
@@ -6,9 +6,14 @@ var w,h;
 var data;
 var drawingdata = []; //an array that keeps the data
 var isloaded = false;
+var X = 0;
+var Y = 0;
+var mouseState = false;
+var v;
+
 
 //the grain class
-function grain(buffer){
+function grain(buffer,position){
 
 	this.now = context.currentTime;
 	this.source = context.createBufferSource();
@@ -19,9 +24,14 @@ function grain(buffer){
 	//connections
 	this.source.connect(this.gain);
 	this.gain.connect(context.destination);
+	
+	this.position = position;
+	this.offset = this.position * (buffer.duration / w);
+	
 	//envelope
-	this.source.start(this.now,Math.random(),1.2); //parameters (when,offset,duration)
+	this.source.start(this.now,this.offset + (Math.random() * 0.3),1.2); //parameters (when,offset,duration)
 	this.gain.gain.setValueAtTime(0.0, this.now);
+
 	this.gain.gain.linearRampToValueAtTime(0.5,this.now + 0.08);
 	this.gain.gain.linearRampToValueAtTime(0,this.now + 1);
 	//garbage collection
@@ -32,10 +42,12 @@ function grain(buffer){
 	
 }
 
-function play(){
+//the voice class
+function voice(position){
 
-	setInterval(function(){
-		var g = new grain(buffer);
+	this.interval = setInterval(function(){
+		var g = new grain(buffer,X);
+
 		grains[graincount] = g;
 		graincount+=1;
 				
@@ -47,6 +59,10 @@ function play(){
 			
 }
 
+voice.prototype.stop = function(){
+	clearInterval(this.interval);
+}
+
 //loading the sound with XML HTTP REQUEST
 var request = new XMLHttpRequest();
 	request.open('GET','2.mp3',true);
@@ -56,15 +72,18 @@ var request = new XMLHttpRequest();
 			buffer = b; //set the buffer
 			data = buffer.getChannelData(0);
 			isloaded = true;
-			
+
+
+			//initialize the processing draw when the buffer is ready
+			var processing = new Processing(canvas,waveformdisplay);
 
 		});
 	};
 request.send();
 
 
-//processing
-function sketch(p){
+//processing - waveform display - canvas 
+function waveformdisplay(p){
 	w = parseInt($('#waveform').css('width'),10);
 	h = parseInt($('#waveform').css('height'),10);
 
@@ -72,6 +91,7 @@ function sketch(p){
 	    var step = Math.ceil( data.length / w );
 	    var amp = h / 2;
 	    
+	    p.background(0);
 	    for( var i=0; i < w; i++ ){
 	        var min = 1.0;
 	        var max = -1.0;
@@ -87,7 +107,7 @@ function sketch(p){
 	                
 	                
 	        }
-	        p.fill(p.color(p.random(255),p.random(255),p.random(255)));
+	        
 	       	p.rect(i,(1+min)*amp,1,Math.max(1,(max-min)*amp));
 	    }
     
@@ -95,32 +115,87 @@ function sketch(p){
 	
 	p.setup = function(){
 		p.size(w,h);
-		//p.background(0);
-		p.frameRate(24);
+		p.background(0);//background black
+		
 		
 		//change the size on resize
 		$(window).resize(function(){
 			w = parseInt($('#waveform').css('width'),10);
 			h = parseInt($('#waveform').css('height'),10);
 			p.size(w,h);
+			//redraw buffer on resize
+			p.stroke(255);
+			drawBuffer();
 
 		});
+		p.stroke(255);
+		drawBuffer();
+		p.noLoop();
+
+
+	};
+	
+
+	
+}
+//processing - grain display
+function grainsdisplay(p){
+	w = parseInt($('#waveform').css('width'),10);
+	h = parseInt($('#waveform').css('height'),10);
+
+	//setup
+	p.setup = function(){
+		p.size(w,h);
+		p.background(0,0);//backgorund black alpha 0
+		p.frameRate(24);
+		p.noLoop();
+		//change the size on resize
+		$(window).resize(function(){
+			w = parseInt($('#waveform').css('width'),10);
+			h = parseInt($('#waveform').css('height'),10);
+			p.size(w,h);
+
+		});	
 		
 	};
 
-	p.draw = function(){
-		//p.background(0);
-		
-		if(isloaded){
-			drawBuffer();
-		}
-		
-		
+	p.mouseMoved = function(){
+		//mouse move event
+		X = p.mouseX;
+		Y = p.mouseY;
 	};
+
+	p.mousePressed = function(){
+		mouseState = true;
+
+		if(mouseState){
+			v = new voice(X);
+		}
+	};
+
+	p.mouseDragged = function(){
+
+	};
+
+	p.mouseReleased = function(){
+		mouseState = false;
+		v.stop();
+	};
+	
+	//draw
+	p.draw = function(){
+
+	};
+
+	
 }
 
 
 $(document).ready(function(){
 	var canvas = document.getElementById('canvas');
-	var processing = new Processing(canvas,sketch);
+
+	//grain display init
+	var canvas2 = document.getElementById('canvas2');
+	var processing = new Processing(canvas2,grainsdisplay);
+	
 });
